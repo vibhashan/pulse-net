f15de195beee871de8306308dc6a68c53c255120
vibhashan <ranhindavibhashana@gmail.com>
2023-07-12 13:57:03 +0530

    initial configuration done

---
+++ main.js
@@ -0,0 +1,51 @@
+/**
+ * Import necessary packages
+ */
+const electron = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
+const path = require('path');
+
+// Enable hot-reload in electron
+try {
+    require('electron-reloader')(module);
+} catch (err) {
+    console.log(err);
+};
+
+// Function to create new window
+function createWindow(height, width) {
+    // Instantiate a BrowserWindow object
+    const window = new BrowserWindow({
+        height: height,
+        width: width
+    });
+
+    // Load the particular URL
+    window.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(() => {
+
+    // Get full height and width of current screen
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+    // Invoke createWindow function by passing height, width as args
+    createWindow(height, width);
+
+}).catch(err => console.log(err));
+
+// Close app window when all windows are closed (Windows OS behaviour)
+app.on('window-all-closed', () => {
+    // Check whether platform is macOS (darwin) or Windows
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+// Invoke createWindow function only when there are no windows
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    };
+})
+
